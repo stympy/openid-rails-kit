187b47bfe61580521ebb741ba496646de6694421
Benjamin Curtis <benjamin.curtis@gmail.com>
2008-05-22 13:17:59 -0700

    Added account creation via openid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < ApplicationController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
-  
-
-  # render new.rhtml
   def new
     @user = User.new
   end
@@ -13,15 +8,19 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     success = @user && @user.save
     if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
+      # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
-      self.current_user = @user # !! now logged in
-            redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      if @user.not_using_openid?
+        self.current_user = @user # !! now logged in
+        redirect_back_or_default('/')
+        flash[:notice] = "Thanks for signing up!"
+      else
+        authenticate_with_open_id(@user.identity_url)
+      end
     else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      flash[:error]  = "We couldn't set up that account, sorry."
       render :action => 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
   include Authentication::ByCookieToken
 
 
-  validates_presence_of     :login
-  validates_length_of       :login,    :within => 3..40
-  validates_uniqueness_of   :login,    :case_sensitive => false
-  validates_format_of       :login,    :with => RE_LOGIN_OK, :message => MSG_LOGIN_BAD
+  validates_presence_of     :login,    :if => :not_using_openid?
+  validates_length_of       :login,    :within => 3..40, :if => :not_using_openid?
+  validates_uniqueness_of   :login,    :case_sensitive => false, :if => :not_using_openid?
+  validates_format_of       :login,    :with => RE_LOGIN_OK, :message => MSG_LOGIN_BAD, :if => :not_using_openid?
 
   validates_format_of       :name,     :with => RE_NAME_OK,  :message => MSG_NAME_BAD, :allow_nil => true
   validates_length_of       :name,     :maximum => 100
@@ -18,13 +18,15 @@ class User < ActiveRecord::Base
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   validates_uniqueness_of   :email,    :case_sensitive => false
   validates_format_of       :email,    :with => RE_EMAIL_OK, :message => MSG_EMAIL_BAD
-
   
+  validates_uniqueness_of   :identity_url, :unless => :not_using_openid?
+
+  validate :normalize_identity_url
 
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :identity_url
 
 
 
@@ -38,9 +40,21 @@ class User < ActiveRecord::Base
     u = find_by_login(login) # need to get the salt
     u && u.authenticated?(password) ? u : nil
   end
+  
+  def not_using_openid?
+    identity_url.blank?
+  end
+
+  def password_required?
+    not_using_openid? && (crypted_password.blank? || !password.blank?)
+  end
 
   protected
     
-
+    def normalize_identity_url
+      self.identity_url = OpenIdAuthentication.normalize_url(identity_url) unless identity_url.blank?
+    rescue URI::InvalidURIError
+      errors.add_to_base("Invalid OpenID URL")
+    end
 
 end
