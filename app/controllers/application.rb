f74cc49b683adc6b10061b633eec924b4e3bb9b5
Benjamin Curtis <benjamin.curtis@gmail.com>
2008-05-22 10:59:01 -0700

    Adding some style

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,4 +7,6 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'ef0abd1ccfc828817c1d3b6905deb19b'
+  
+  filter_parameter_logging :password
 end
